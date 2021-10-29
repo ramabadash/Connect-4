2296b2f8452827b7df1723a0c51e94cf41870584
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-10-29 14:59:56 +0300

    # Controller class file

---
+++ src/controller.js
@@ -0,0 +1,24 @@
+import Connect4 from './model';
+import View from './view';
+
+// The class is responsible for linking the view to the model. He is assisted for this in a class event.
+//Adds to each "event" in view or in model a function on which the "trigger" is made.
+
+class Controller {
+  constructor() {
+    this.model = new Connect4();
+    this.view = new View();
+
+    this.view.playEvent.addListener(move => { this.model.play(move); }); //Making a move
+
+    this.model.updateCellEvent.addListener(data => { this.view.updateCell(data); }); //Update the move in the interface
+    this.model.victoryEvent.addListener(winner => { this.view.victory(winner); }); //A victory was achieved
+    this.model.drawEvent.addListener(() => { this.view.draw(); }); //A draw was reached
+  }
+  //build the board
+  run() {
+    this.view.render();
+  }
+}
+
+export default Controller;
